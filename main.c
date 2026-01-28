a31d14913cea4651a4b2f6e274e0ba71e2319e71
NeMurash <dimabelida5@gmail.com>
2026-01-29 00:33:35 +0100

    Idk

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+#define GRID_W 16
+#define GRID_H 16
+
+enum Glyphs {
+	GLYPH_FILL = '#',
+	GLYPH_MINE = 'X',
+	GLYPH_FLAG = 'F',
+	GLYPH_NONE = ' ',
+};
+
+struct Cell {
+	char glyph;
+};
+
 int main(void) {
-	printf("Hello, World!\n");
+	struct Cell cells[GRID_H][GRID_W];
+	for (int y=0; y<GRID_H; y++) {
+		for (int x=0; x<GRID_W; x++) {
+			cells[y][x].glyph = GLYPH_FILL;
+			printf("%c ", cells[y][x].glyph);
+		}
+		printf("\n");
+	}
+
 	return 0;
 }
