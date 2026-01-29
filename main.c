e311bf4c5648814cd2b4d8394938bf472c1aca44
NeMurash <dimabelida5@gmail.com>
2026-01-29 21:45:27 +0100

    Terminal shenanigans

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
 
 #define GRID_W 16
 #define GRID_H 16
@@ -14,7 +16,14 @@ struct Cell {
 	char glyph;
 };
 
+void initTerminal();
+void quitTerminal();
+
+struct termios oldTerminal, newTerminal;
+
 int main(void) {
+	initTerminal();
+
 	struct Cell cells[GRID_H][GRID_W];
 	for (int y=0; y<GRID_H; y++) {
 		for (int x=0; x<GRID_W; x++) {
@@ -24,5 +33,47 @@ int main(void) {
 		printf("\n");
 	}
 
+	char c;
+	int dx = 0;
+	int dy = 0;
+	while ((c = getchar()) != 'q') {
+		switch (c) {
+			case 'w':
+				printf("\033[1A");
+				dy++;
+				break;
+			case 's':
+				printf("\033[1B");
+				dy--;
+				break;
+			case 'd':
+				printf("\033[2C");
+				dx++;
+				break;
+			case 'a':
+				printf("\033[2D");
+				dx--;
+				break;
+			default: break;
+		}
+	}
+
+	quitTerminal();
+
 	return 0;
+}
+
+void initTerminal() {
+	tcgetattr(STDIN_FILENO, &oldTerminal);
+
+	newTerminal = oldTerminal;
+
+	newTerminal.c_lflag &= ~(ICANON);
+	newTerminal.c_lflag &= ~(ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &newTerminal);
+}
+
+void quitTerminal() {
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldTerminal);
 }
